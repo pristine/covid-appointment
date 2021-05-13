628dfd89f088f8bbba5cc5dd0ddede68401336e9
EdwinJ0124 <35356243+EdwinJ0124@users.noreply.github.com>
2021-05-13 01:29:05 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,6 @@ import app from './app';
 const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
 const start = async () => {
-  try {
-    require('./db');
-  } catch (error) {
-    // eslint-disable-next-line
-    console.error('Unable to connect to databases');
-    // Exit process if there are no db connections
-    process.exit(1);
-  }
-
   const server = http.createServer(app);
 
   server.listen(process.env.PORT || 8080, () => {
