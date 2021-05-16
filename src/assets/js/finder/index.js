b8c984b9ad76ee51d6bb8287d16b46994a3df791
EdwinJ0124 <35356243+EdwinJ0124@users.noreply.github.com>
2021-05-16 15:21:14 -0700

    Update index.js

--- src/assets/js/finder/index.js
+++ src/assets/js/finder/index.js
@@ -26,6 +26,7 @@ const findVaccineLocations = async ( vaccine, lat, long, radius ) => {
         mode: 'cors',
         headers: {
             "accept": "application/json",
+            "origin": "https://api.us.castlighthealth.com/",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         }
     });
