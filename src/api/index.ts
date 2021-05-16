b3f624591b74a55e3bfbcd7ec67e6ff1a7ef1366
EdwinJ0124 <35356243+EdwinJ0124@users.noreply.github.com>
2021-05-16 15:32:39 -0700

    add vaccine route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import location from './location'
+import vaccine from './vaccine'
 
 const api = express.Router()
 
 api.use('/location', location);
+api.use('/vaccine', vaccine);
 
 export default api;
