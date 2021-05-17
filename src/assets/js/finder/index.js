df6542bc76111e7a9a56b40845705066bbdc11a9
EdwinJ0124 <35356243+EdwinJ0124@users.noreply.github.com>
2021-05-16 18:17:40 -0700

    update vaccine type

--- src/assets/js/finder/index.js
+++ src/assets/js/finder/index.js
@@ -3,7 +3,7 @@ const apiUrl = `https://api.us.castlighthealth.com/vaccine-finder/v1/provider-lo
 const vaccines = {
     "moderna": "779bfe52-0dd8-4023-a183-457eb100fccc",
     "pfizer": "a84fb9ed-deb4-461c-b785-e17c782ef88b",
-    "J&J": "784db609-dc1f-45a5-bad6-8db02e79d44f",
+    "johnson & johnson": "784db609-dc1f-45a5-bad6-8db02e79d44f",
 }
 
 const findVaccineLocations = async ( vaccine, lat, long, radius ) => {
